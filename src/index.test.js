1a1479ffc63509c9618da44db6b9d14e08941aa3
Mecxi Musa <mecximusa@gmail.com>
2019-08-30 13:09:02 +0200

    Fix broken test

--- src/index.test.js
+++ src/index.test.js
@@ -14,7 +14,7 @@ describe('index.html', ()=>{
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window){
       const p = window.document.getElementsByTagName('p')[0];
-      expect(p.innerHTML).to.equal("Hello World");
+      expect(p.innerHTML).to.equal("Hello World!");
       done();
       window.close();
     });
